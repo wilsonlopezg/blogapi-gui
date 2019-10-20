561321ffa76a7607074499bdabc95d7103c866f6
Wilson Lopez <wlopezgar@gmail.com>
2019-10-19 23:39:35 -0400

    Update Login, Post

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public/'));
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/public/', 'index.html'));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port);
+console.log(`Server started on port ${port}`);
